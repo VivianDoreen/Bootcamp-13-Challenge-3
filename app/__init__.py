489184e42dd709446ce21351b21e914ba9f5b1fc
Nabulo Vivian <nabulovivian2014@gmail.com>
2018-10-31 13:59:50 -0700

    remove app from method create_app

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,10 @@ from flask_api import FlaskAPI
 from config import application_config
 from database import DatabaseConnection
 
+app = FlaskAPI(__name__)
 
 
 def create_app(config_name):
-    app = FlaskAPI(__name__)
     APP_ROOT = os.path.dirname(app.instance_path)
     app.config.from_object(application_config[config_name])
     app.config.from_pyfile(APP_ROOT+'/config.py')
