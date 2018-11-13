360f8c7121953b4a24e3a2ad774e135b449d690c
Nabulo Vivian <nabulovivian2014@gmail.com>
2018-11-13 13:39:33 -0800

    remove some comments

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,9 @@ import os
 from flask import Flask
 from config import application_config
 from database import DatabaseConnection
-# from database import create_tables, connect_db
 
 app = Flask(__name__)
 from app.views import users
-# print(users.env)
 database_connection = DatabaseConnection()
 database_connection.create_tables()
-# print(database_connection.db)
-# print(database_connection.co)
-# print(database_connection.get_ev)
-# print(application_config['DevelopmentEnv'].DATABASE)
-# print(application_config['TestingEnv'].DATABASE)
+
