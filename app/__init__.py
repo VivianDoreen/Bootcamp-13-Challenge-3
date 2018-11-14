ce775a15339bf7c688fc3db6a42bc7c2d021d4ec
taakajoan <taakajoan42@gmail.com>
2018-11-14 10:29:21 -0800

    add cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,14 @@ import os
 from flask import Flask
 from config import application_config
 from database import DatabaseConnection
+from flask-cors import CORS
 
 app = Flask(__name__)
-from app.views import users
+CORS(app)
 database_connection = DatabaseConnection()
 database_connection.create_tables()
+database_connection.create_admin()
+from app.views import users
+from app.views import products
+from app.views import sales
 
