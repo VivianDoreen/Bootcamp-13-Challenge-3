30207f0e3c8edb76f49a7275c76f58ac451c0170
VivianDoreen <nabulovivian2014@gmail.com>
2018-11-14 16:41:56 -0800

    remove flask-cors

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,9 @@ from flask import Flask
 from config import application_config
 #My app
 from database import DatabaseConnection
-from flask-cors import CORS
-
 app = Flask(__name__)
-CORS(app)
 database_connection = DatabaseConnection()
 database_connection.create_tables()
-database_connection.create_admin()
 from app.views import users
 from app.views import products
 from app.views import sales
